472827d3861bb8b4c066d222406da9d591b218da
Charles Gitau <gitauwairimu@gmail.com>
2025-08-08 14:53:09 +0300

    remove outputs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -150,22 +150,3 @@ resource "google_cloud_run_service_iam_member" "public_access" {
     google_cloud_run_service.insight_agent
   ]
 }
-
-# ========================
-# OUTPUTS
-# ========================
-output "artifact_registry" {
-  value = google_artifact_registry_repository.insight_agent.name
-}
-
-output "cloud_run_url" {
-  value = google_cloud_run_service.insight_agent.status[0].url
-}
-
-output "state_bucket" {
-  value = google_storage_bucket.tf_state.name
-}
-
-output "service_account_email" {
-  value = google_service_account.insight_agent.email
-}
