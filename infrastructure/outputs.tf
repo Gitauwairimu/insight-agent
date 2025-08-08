a4077a617c10362c0959eaf4ef5e3f7fd4aa92ca
Charles Gitau <gitauwairimu@gmail.com>
2025-08-08 17:05:57 +0300

    update backend outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -6,9 +6,10 @@ output "cloud_run_url" {
   value = google_cloud_run_service.insight_agent.status[0].url
 }
 
-#output "state_bucket" {
-#  value = google_storage_bucket.tf_state.name
-#}
+output "state_bucket" {
+  value = google_storage_bucket.tf_state.name
+}
+
 
 output "service_account_email" {
   value = google_service_account.insight_agent.email
