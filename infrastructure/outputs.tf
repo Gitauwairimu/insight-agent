a34c4a6f6f1f461d2f4b1ae75d3b3f4c62720efe
Charles Gitau <gitauwairimu@gmail.com>
2025-08-08 14:52:19 +0300

    added state bucket as output

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,12 +1,15 @@
-# infrastructure/outputs.tf
-output "artifact_registry_repository" {
+output "artifact_registry" {
   value = google_artifact_registry_repository.insight_agent.name
-}
-
-output "service_account_email" {
-  value = google_service_account.insight_agent.email
 }
 
 output "cloud_run_url" {
   value = google_cloud_run_service.insight_agent.status[0].url
+}
+
+output "state_bucket" {
+  value = google_storage_bucket.tf_state.name
+}
+
+output "service_account_email" {
+  value = google_service_account.insight_agent.email
 }
