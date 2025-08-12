df5a7a43d2784b422559d34e5d1fb052914e36fe
Charles Gitau <gitauwairimu@gmail.com>
2025-08-12 03:46:21 +0300

    document the app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
+# FastAPI App Initialization
 app = FastAPI()
 
-
+#     Defines the expected request format with a single text field
 class TextRequest(BaseModel):
     text: str
 
-
+# The endpoint analyzes the provided text and returns statistics.
 @app.post("/analyze")
 def analyze(request: TextRequest):
     text = request.text
